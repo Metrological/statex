130b067c4af53a6666b353cacce690f3983119bd
basvanmeurs <basvanmeurs@live.com>
2018-03-05 13:34:59 +0100

    feat(patch): check input

--- src/Base.js
+++ src/Base.js
@@ -5,13 +5,18 @@ class Base {
     }
 
     static patchObject(obj, settings) {
-        let names = Object.keys(settings)
-        for (let i = 0, n = names.length; i < n; i++) {
-            let name = names[i]
+        if (!Utils.isObjectLiteral(settings)) {
+            console.error("Settings must be object literal")
+        } else {
+            let names = Object.keys(settings)
+            for (let i = 0, n = names.length; i < n; i++) {
+                let name = names[i]
 
-            this.patchObjectProperty(obj, name, settings[name])
+                this.patchObjectProperty(obj, name, settings[name])
+            }
         }
     }
+
 
     static patchObjectProperty(obj, name, value) {
         let setter = obj.setSetting || Base.defaultSetter;
